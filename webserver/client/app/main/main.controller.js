3e82ec5d96bda951193505c78e31cc0ca63f460f
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-11-03 18:46:33 +0800

    remove unnecseary

--- webserver/client/app/main/main.controller.js
+++ webserver/client/app/main/main.controller.js
@@ -2,10 +2,4 @@
 
 angular.module('webserverApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
   });
