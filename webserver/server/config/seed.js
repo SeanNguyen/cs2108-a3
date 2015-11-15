f97e86e73a9f07a94680fdbcee0a971f0811f8b6
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-11-15 17:28:20 +0800

    added seed data

--- webserver/server/config/seed.js
+++ webserver/server/config/seed.js
@@ -4,28 +4,30 @@
  */
 
 'use strict';
+var fs = require('fs');
+var Photo = require('../api/photo/photo.model');
 
-var Thing = require('../api/thing/thing.model');
-
-
-Thing.find({}).remove(function() {
-  Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-  }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-  }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
+Photo.find({'user' : 'system'}).remove(function() {
+  //read svg file
+  fs.readFile('../train.csv', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    var lines = data.split("\r\n");
+    for (var i = lines.length - 1; i >= 0; i--) {
+      var line = lines[i];
+      if(line.length == 0)
+        continue;
+      var args = line.split(",");
+      var name = args[0];
+      args.splice(0, 1)
+      Photo.create({
+        name: name,
+        description: 'System Photo',
+        user: 'system',
+        path: 'uploads/' + name,
+        tags: args
+      });
+    };
   });
-});
+})
